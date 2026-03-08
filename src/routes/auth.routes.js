66c1813644bcffd901a0e739183b68c147374705
NguynKng <nguynkhang2109@gmail.com>
2026-03-08 17:39:39 +0700

    change auth filename in controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const AuthController = require('../controllers/AuthController');
+const AuthController = require('../controllers/auth.controller');
 const { authenticate } = require('../middlewares/auth.middleware');
 
 const router = express.Router();
